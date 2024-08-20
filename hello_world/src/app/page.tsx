b6db898d6211bf81c1e41aef22227ff747cc4b61
Habib-bhai <habibahmed918131@gmail.com>
2024-08-20 23:27:35 +0500

    STEP-00 HELLO WORLD IN NEXT.JS

---
+++ hello_world/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+      <div>
+
+      <h1>HELLO WORLD</h1>
+      <h2>from  BRO HABIB</h2>
+
+      </div>
+
+);
+}
