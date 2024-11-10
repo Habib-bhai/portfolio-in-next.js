f5af3e3fbd6371ff243d9bd6c1997448a9f52ab7
Habib-bhai <habibahmed918131@gmail.com>
2024-11-10 18:18:40 +0500

    tweaks in responsiveness

--- portfolio-nextjs-with-tailwindcss/src/components/hero-section/HeroSection.tsx
+++ portfolio-nextjs-with-tailwindcss/src/components/hero-section/HeroSection.tsx
@@ -33,21 +33,24 @@ export default function HeroSection() {
   return (
     <div className="w-full overflow-x-hidden">
     
-    <div className="ml-3 w-[100%] flex   justify-center items-center flex-wrap md:flex-nowrap ">
+    <div className="md:ml-3  w-full flex   justify-center items-center flex-wrap md:flex-nowrap ">
+
     {/* info and my pic */}
-    <div className="my-10 flex justify-center font-jost  items-center md:items-start flex-col">
-    <h2 className="text-3xl">HI, I&apos;AM A FREELANCER</h2>
-    <h1 className="mt-3 mb-6 text-3xl w-[320px] md:text-5xl  md:w-[520px] font-sans text-[#5271ff] font-extrabold">{text} <Cursor/> 
+    <div className="my-10  flex justify-center font-jost  items-center md:items-start flex-col">
+    <h2 className="text-xl md:text-3xl">HI, I&apos;AM A FREELANCER</h2>
+
+    <h1 className="mt-3 mb-6 text-xl w-[320px] md:text-5xl  md:w-[520px] font-sans text-[#5271ff] font-extrabold text-center md:text-start">{text} <Cursor/> 
     </h1> 
-    <p className="font-jost  w-96">I am a Frontend developer especializing in next.js and JamStack. Explore my <Link href={"/porjects"} className="text-[#5271ff] ">Portfolio</Link>, <Link className="text-[#5271ff]" href={"/resume"}>Resume.</Link></p>
+    <p className="font-jost  w-80 text-sm text-center">I am a Frontend developer especializing in next.js and JamStack. Explore my <Link href={"/porjects"} className="text-[#5271ff] ">Portfolio</Link>, <Link className="text-[#5271ff]" href={"/resume"}>Resume.</Link></p>
 
-    <div className=" flex gap-4 mt-5">
+    <div className=" flex w-full justify-center items-center md:justify-start gap-4 mt-5">
       {/* btns */}
-      <Link href={"/projects"}><button className="relative text-white font-semibold font-jost h-10 w-32 md:h-12 md:w-40 rounded-[100px] bg-[#292929] after:contents-[''] after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-transparent after:scale-0 hover:after:scale-100 hover:after:bg-[#5271ff]/80 after:transition-all after:duration-300 after:ease-in-out after:rounded-3xl    ">View Portfolio</button></Link>
-     <Link href={"/resume"}> <button className="relative text-white font-semibold font-jost h-10 w-32 md:h-12 md:w-40 rounded-[100px] bg-[#292929] after:contents-[''] after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-transparent after:scale-0 hover:after:scale-100 hover:after:bg-[#5271ff]/80 after:transition-all after:duration-300 after:ease-in-out after:rounded-3xl  ">View Resume </button> </Link>
+      <Link href={"/projects"}><button className="relative text-[12px] md:text-lg text-white font-semibold font-jost h-10 w-24 md:h-12 md:w-40 rounded-[100px] bg-[#292929] after:contents-[''] after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-transparent after:scale-0 hover:after:scale-100 hover:after:bg-[#5271ff]/80 after:transition-all after:duration-300 after:ease-in-out after:rounded-3xl    ">View Portfolio</button></Link>
+     <Link href={"/resume"}> <button className="relative text-[12px] md:text-lg  text-white font-semibold font-jost h-10 w-24 md:h-12 md:w-40 rounded-[100px] bg-[#292929] after:contents-[''] after:w-full after:h-full after:absolute after:top-0 after:left-0 after:bg-transparent after:scale-0 hover:after:scale-100 hover:after:bg-[#5271ff]/80 after:transition-all after:duration-300 after:ease-in-out after:rounded-3xl  ">View Resume </button> </Link>
     </div>
 
     </div>
+
 
       <div  className="w-96 h-96 md:w-[400px] md:h-[400px] relative flex justify-center items-center ">
         <div ref={imgDiv} className="absolute top-16 z-[-1] w-64 h-64 md:w-[300px] md:h-[300px] bg-[#5271ff]"></div>
